3582b7b79e8d57364f8b0e0e59b76577660d0c23
罗勇 <51957114+Rayong612@users.noreply.github.com>
2020-01-13 20:27:24 +0800

    纵向横向滚动条的使用

---
+++ 6/pages/index/index.js
@@ -0,0 +1,106 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+          
+    toView: 'demo2',
+    slideWidth: '',//滑块宽
+    slideLeft: 0,//滑块位置
+    totalLength: '',//当前滚动列表总长
+    slideShow: false,
+    slideRatio: ''
+  },
+
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+onload:function(){
+
+},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
