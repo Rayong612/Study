e2cd75a5d6d71e9e270a4af05a6a8324537e6500
罗勇 <51957114+Rayong612@users.noreply.github.com>
2020-02-06 23:43:10 +0800

    第一次使用云函数

---
+++ text2/miniprogram/pages/base/base.js
@@ -0,0 +1,79 @@
+// pages/base/base.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+sunBtn:function(){
+  wx.cloud.callFunction({
+    name:'sum',
+    data:{
+      a:3,
+      b:2
+    }
+  }).then(res=>{
+    console.log(res)
+  }).catch(err=>{
+    console.log(err)
+  })
+},
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
